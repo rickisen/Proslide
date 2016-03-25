a40f1c73c1c7614468cda63184a6fb63da6e7869
rickisen <rickard.spam@gmail.com>
2016-03-25 19:03:18 +0100

    Applied some more pretty styling

--- public/templates/header.php
+++ public/templates/header.php
@@ -3,6 +3,8 @@
 <head>
   <meta charset="utf-8">
   <title>Rickisen's School Projects</title>
+  <link href='https://fonts.googleapis.com/css?family=Arimo' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Raleway:400,700' rel='stylesheet' type='text/css'>
   <link rel="stylesheet" href="/stylesheets/nomalize.css" type="text/css"  charset="utf-8">
   <link rel="stylesheet" href="/stylesheets/main.css" type="text/css"  charset="utf-8">
 </head>

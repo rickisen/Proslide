69ab6c2a2ee6d0dab9d859e4dafe14edaf4b6c79
rickisen <rickard.spam@gmail.com>
2016-03-28 13:25:02 +0200

    fixes path bug

--- public/classes/XmlDb.class.php
+++ public/classes/XmlDb.class.php
@@ -8,28 +8,28 @@ class XmlDb{
 
   public static function getInstance(){
     if(!self::$instance){
-      $config = parse_ini_file('/config.ini');
+      $config = parse_ini_file('config.ini');
 
       // load and instantize the xml file
       $sxeFile = simplexml_load_file($config['dbPath']);
       self::$instance = new SimpleXMLElement($sxeFile->asXML());
 
       return self::$instance;
-    }else{
+    } else {
       return self::$instance;
     }
   }
 
   public static function getUsers(){
     if(!self::$users){
-      $config = parse_ini_file('/config.ini');
+      $config = parse_ini_file('config.ini');
 
       // load and instantize the xml file
       $sxeFile = simplexml_load_file($config['usersPath']);
       self::$users = new SimpleXMLElement($sxeFile->asXML());
 
       return self::$users;
-    }else{
+    } else {
       return self::$users;
     }
   }

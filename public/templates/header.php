fcca77dd01220ae9b16dcd80b51570ee51158c2d
rickisen <rickard.spam@gmail.com>
2016-04-02 00:48:33 +0200

    fixes bug introduced by spelling misstake

--- public/templates/header.php
+++ public/templates/header.php
@@ -5,7 +5,7 @@
   <title>Rickisen's School Projects</title>
   <link href='https://fonts.googleapis.com/css?family=Arimo' rel='stylesheet' type='text/css'>
   <link href='https://fonts.googleapis.com/css?family=Raleway:400,700' rel='stylesheet' type='text/css'>
-  <link rel="stylesheet" href="/stylesheets/nomalize.css" type="text/css"  charset="utf-8">
+  <link rel="stylesheet" href="/stylesheets/normalize.css" type="text/css"  charset="utf-8">
   <link rel="stylesheet" href="/stylesheets/main.css" type="text/css"  charset="utf-8">
 </head>
 <body>

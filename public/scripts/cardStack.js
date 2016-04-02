590a8baa37987ae014ba252e0a527d8e5dd11608
rickisen <rickard.spam@gmail.com>
2016-04-02 21:57:30 +0200

    uses new reVielCard function, removes old, and general beutifying of code

--- public/scripts/cardStack.js
+++ public/scripts/cardStack.js
@@ -1,107 +1,53 @@
 "use strict"
-var cardHeight = 0; // should preffably be higher than the highest card
+var cardHeight = 0; 
 var cardHeadHeightMax = 32;
 var cardHeadHeightMin = 5; 
 var cardHeadHeight = ( cardHeadHeightMax + cardHeadHeightMin ) / 2 ;
 
-setupPos(); // sets the correct position prop on all relevant elements
+init(); // sets the correct position prop on all relevant elements
 addKeyEvents();
-stackAll(); // Shows the head of all the cards in the stack, and adds eventlisteners to the cards 
 
-function setupPos() {
+function init() {
 	var stacks = document.getElementsByClassName("stack");
 	for (var i = 0, len = stacks.length; i < len; i++) {
 		stacks[i].style.position = "relative";
-
-		var zIndex = 1;
+		
+		// get all cards in this stack, needed for calculating its height
 		var cards = stacks[i].getElementsByClassName("card");
-		for (var i = 0, len = cards.length; i < len; i++) {
-			cards[i].style.position = "absolute";
-			cards[i].style.zIndex = zIndex++;
-		}
-	}
-}
-
-// Stack all cards in all stacks, and add relevant listeners to the cards.
-// Used when seting up the script, and when the user clicks a card that is already revieled
-function stackAll() {
-	var stacks = document.getElementsByClassName("stack");
-	for (var i = 0, len = stacks.length; i < len; i++) {
-		// Get all cards in this stack
-		var cards = stacks[i].getElementsByClassName("card"); 
 
 		// calculate the optimal card height
-		cardHeight = calcOptCardHieght(cards.length, 90); 
+		cardHeight = calcOptCardHieght(cards.length, 180); 
 
 		// calculate this stacks height
 		var stackHeight = cardHeight - cardHeadHeightMax + (cardHeadHeightMax * stacks[i].children.length ); 
 		stacks[i].style.height = "" + stackHeight + "px"; 
-		
-		// // loop through all cards and stack them, and add listeners to reviel them
-		// for (var i = 0, len = cards.length; i < len; i++) {
-		// 	cards[i].style.top = "" + (cardHeadHeight * i ) + "px";
 
-		// 	// replace the listeners, 
-		// 	cards[i].getElementsByTagName("header")[0].removeEventListener("click", reVeilListener);
-		// 	cards[i].getElementsByTagName("header")[0].removeEventListener("click", stackAll);
-		// 	cards[i].getElementsByTagName("header")[0].addEventListener("click", reVeilListener);
-
-		// 	// Unmark the card as revield in css
-		// 	cards[i].className = cards[i].className.replace( new RegExp('(?:^|\\s)reVeiledCard(?!\\S)') ,'');
-
-		// 	// minimize it, and make sure its scroll is reset
-		// 	cards[i].style.height = "" + Math.round(cardHeadHeight) + "px";
-		// 	cards[i].scrollTop = 0;
-		// 	cards[i].style.overflowY = "hidden";
-		// }
-
-		// reveil the last card in the stack
+		// loop throough all cards and give them an incrmenting z-index, and event listeners
+		var zIndex = 1;
+		for (var i = 0, len = cards.length; i < len; i++) {
+			cards[i].style.position = "absolute";
+			cards[i].style.zIndex = zIndex++;
+			cards[i].getElementsByTagName("header")[0].addEventListener("click", reVeilListener);
+		}
+	
+		// finaly stack all cards by calling the reveil card 
+		// function on the last card in the stack
 		reVielCard(cards[cards.length - 1]); 
 	}
 }
 
-function reVielCard1(cardToReveil) {
-	// loop through cardToReveil's siblings and reviel 
-	// "cardToReveil" by moving its sibling after him 
-	// down the correct amount of pixles
-	var siblings = cardToReveil.parentNode.children;
-	var foundElem = false;
-	for (var i = 0, len = siblings.length; i < len; i++) {
-		if (foundElem) {
-			siblings[i].style.top = "" + ((cardHeadHeight * i ) + cardHeight - cardHeadHeight ) + "px";
-		} else {
-			siblings[i].style.top = "" + (cardHeadHeight * i ) + "px";
-		}
-
-		// allways reset the scroll
-		siblings[i].scrollTop = 0;
-
-		if (siblings[i] == cardToReveil) {
-			foundElem = true; // so that the next card will be moved down
-			// class the card as the currently reveiled card, give it the correct height, add suitable listener
-			siblings[i].className += " reVeiledCard"
-			siblings[i].style.height = "" + cardHeight + "px";
-			siblings[i].getElementsByTagName("header")[0].removeEventListener("click", reVeilListener); 
-			siblings[i].getElementsByTagName("header")[0].addEventListener("click", stackAll);
-			siblings[i].style.overflowY = "auto"; // show scrollbars if need be
-		} else {
-			// Unmark the card as revield
-			siblings[i].className = siblings[i].className.replace( new RegExp('(?:^|\\s)reVeiledCard(?!\\S)') ,'');
-			siblings[i].style.height = "" + Math.round(cardHeadHeight) + "px";
-			siblings[i].getElementsByTagName("header")[0].removeEventListener("click", stackAll); 
-			siblings[i].getElementsByTagName("header")[0].addEventListener("click", reVeilListener);
-			siblings[i].style.overflowY = "hidden";// never show scrollbars on hidden cards
-		}
-	}
-
-	// set the new height of the parent stack
-	var activeStackHeight = ( cardHeight - cardHeadHeight ) + ( cardHeadHeight * ( cardToReveil.parentNode.children.length )) ; 
-	cardToReveil.parentNode.style.height = "" + activeStackHeight + "px"; 
-}
-
 function reVielCard(cardToReveil) {
 	var siblings = cardToReveil.parentNode.children;
-	// find the cards position amongs his sibblings
+
+	// if this card already is revieled, the user 
+	// probably pushed the head of the revieled card in 
+	// an attempt to minimize it, so we reviel the 
+	// last card in the stack instead
+	if (cardToReveil.classList.contains("reVeiledCard")) {
+		cardToReveil = siblings[siblings.length - 1]; 
+	}
+
+	// find the cards position amongs his siblings
 	for (var i = 0, len = siblings.length; i < len; i++) {
 		if (siblings[i] == cardToReveil) {
 			var ctrPos = i ; 
@@ -113,7 +59,6 @@ function reVielCard(cardToReveil) {
 	// and change those element's "settings"
 	for (var i = 0; i < ctrPos; i++ ) {
 		siblings[i].style.top = "" + i * cardHeadHeightMax + "px";
-		// siblings[i].style.top = "" + calcHeadHeight(i , ctrPos) + "px";
 		handleStackedCardSettings(siblings[i]); 
 	}
 
@@ -128,17 +73,18 @@ function reVielCard(cardToReveil) {
 	}
 
 	// then move the next card down enough so that the whole CTR is seen
-	siblings[ctrPos + 1].style.top = "" + cardHeight + "px";
-
-	for (var i = siblings.length - 1 ; i > ctrPos ; i-- ) {
-		// siblings[i].style.top = "" + calcHeadHeight(i , sibblings.length) + "px";
-		var top = cardHeight - cardHeadHeightMax +  ( i * cardHeadHeightMax ); 
+	siblings[ctrPos + 1].style.top = "" + cardHeight - cardHeadHeight + "px";
+	for (var i = ctrPos + 1 ; i < siblings.length; i++ ) {
+		let top = cardHeight - cardHeadHeightMax +  ( i * cardHeadHeightMax ); 
 		siblings[i].style.top = "" + top + "px";
 		handleStackedCardSettings(siblings[i]); 
 	}
+
 }
 
 function reVeilListener(event) {
+	// the target could be any element inside the 
+	// element that had the listener attached to it.
 	reVielCard(findParentCard(event.target));
 }
 
@@ -156,28 +102,27 @@ function findParentCard(suspect) {
 	return findParentCard(suspect.parentNode);
 }
 
-// Unmarks the card as revield
-function handleStackedCardSettings(stackedCard) {
-	stackedCard.className = stackedCard.className.replace( new RegExp('(?:^|\\s)reVeiledCard(?!\\S)') ,'');
-	stackedCard.style.height = "" + cardHeadHeightMax + "px"; 
-	stackedCard.getElementsByTagName("header")[0].removeEventListener("click", stackAll); 
-	stackedCard.getElementsByTagName("header")[0].addEventListener("click", reVeilListener);
-	stackedCard.style.overflowY = "hidden";// never show scrollbars on stacked cards
-}
-
-// Marks a card as revield, CTR = Card To Reviele
+// Marks and styles a card as revield, CTR = Card To Reviele
 function handleCtrSettings(cardToReveil) {
 	cardToReveil.className += " reVeiledCard"
 	cardToReveil.style.height = "" + cardHeight + "px";
-	cardToReveil.getElementsByTagName("header")[0].removeEventListener("click", reVeilListener); 
-	cardToReveil.getElementsByTagName("header")[0].addEventListener("click", stackAll);
 	cardToReveil.style.overflowY = "auto"; // show scrollbars if need be
+}
+
+// Unmarks and styles the card as revield
+function handleStackedCardSettings(stackedCard) {
+	stackedCard.className = stackedCard.className.replace( new RegExp('(?:^|\\s)reVeiledCard(?!\\S)') ,'');
+	stackedCard.style.height = "" + cardHeadHeightMax + "px"; 
+	stackedCard.style.overflowY = "hidden";// never show scrollbars on stacked cards
+	stackedCard.scrollTop = 0;// allways reset the scroll
 }
 
 function reVielNextCard(direction) {
 	var reVeiledCard = document.getElementsByClassName("reVeiledCard")[0];
 	var siblings = reVeiledCard.parentNode.children; 
 
+	// find the currently revieled card and reviel his nieghbor
+	// in a direction.
 	for (var i = 0, len = siblings.length; i < len; i++) {
 		if (siblings[i] == reVeiledCard) {
 			if (direction == "down" && len > i + 1 ) {
@@ -201,23 +146,12 @@ function addKeyEvents() {
 			case 74 :
 				reVielNextCard("down"); 
 				break;
-
-			default:
-
 		}
 	}, false); 
 }
 
-// function for calculating how much of a cards 
-// head to be visible in a stack, 
-// the fruther away a card is from the revieled 
-// card the less we should see of its head
-function calcHeadHeight(currentPos, LastPos ) {
-	var rangeMax = cardHeadHeight - cardHeadHeightMin; 
-	var rangeMin = rangeMax / LastPos;
-	return rangeMin * currentPos + cardHeadHeightMin; 
-}
-
+// calculates a "fitting" height of a card based on the 
+// current window height and a specified offset
 function calcOptCardHieght(siblings, aboveBellow = 0) {
 	var windowHeight = "innerHeight" in window 
 		? window.innerHeight

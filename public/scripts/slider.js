6771451740c35072b72d84c87dcb4f5977a4edc7
rickisen <rickard.spam@gmail.com>
2016-04-03 15:33:58 +0200

    adds black bars around the slider image for nicer navigation

--- public/scripts/slider.js
+++ public/scripts/slider.js
@@ -10,12 +10,24 @@ ajax.send();
 var buttons = document.getElementsByClassName("sliderBtn"); 
 for (var i = 0, len = buttons.length; i < len; i++) {
 	buttons[i].addEventListener("click", function(event){
-		var projId = event.target.getAttribute("projId");
-		var direction = event.target.getAttribute("direction");
+		var projId    = findParent(event.target, "slider").getAttribute("projId");
+		var direction = findParent(event.target, "sliderBtn").getAttribute("direction");
 
 		slideImage(projId, direction);
 	}); 
 	
+}
+
+// recursive function that tries to return the first 
+// parentNode that has a given class 
+function findParent(suspect, parentClass) {
+	if (suspect.classList.contains(parentClass)) {
+		return suspect;
+	} else if (suspect != document) {
+		return findParent(suspect.parentNode, parentClass);
+	} else {
+		return null;
+	}
 }
 
 // add listeners to the arrow keys
@@ -35,16 +47,14 @@ window.addEventListener("keyup", function(event){
 				slideImage(projId, "prev"); 
 			}
 			break;
-
-		default:
 	}
 }, false); 
 
 function activeProj() {
 	var activeCard = document.getElementsByClassName("reVeiledCard")[0]; 
-	var sliderBtn = activeCard.getElementsByTagName("button"); 
-	if (sliderBtn.length > 0 ){
-		return sliderBtn[0].getAttribute("projId") ; 
+	var slider = activeCard.getElementsByClassName("slider"); 
+	if (slider.length > 0 ){
+		return slider[0].getAttribute("projId") ; 
 	} else {
 		return null;
 	}
@@ -57,7 +67,7 @@ function slideImage(projId, direction = "next") {
 	// direction can be next or prev.
 	var proj         = document.getElementById("proj-" + projId) ;
 	var slider       = proj.getElementsByClassName("slider")[0];
-	var currentImage = slider.getElementsByTagName("img")[0];
+	var currentImage = slider.getElementsByClassName("sliderImage")[0];
 	var images       = getProjImagesFromXml(projId);
 
 	var currentImageId = currentImage.getAttribute("xmlId")
@@ -82,7 +92,7 @@ function slideImage(projId, direction = "next") {
 	var nextImage = images[nextImageIndex];
 
 	// replace the current Images attributes to the next image
-	currentImage.src = nextImage.getElementsByTagName("src")[0].innerHTML;
+	currentImage.style.backgroundImage = "url(" + nextImage.getElementsByTagName("src")[0].innerHTML + ")";
 	currentImage.setAttribute("xmlid", nextImage.id); 
 	slider.getElementsByClassName("title")[0].textContent = nextImage.getElementsByTagName("title")[0].innerHTML;
 	slider.getElementsByClassName("caption")[0].textContent = nextImage.getElementsByTagName("caption")[0].innerHTML;
--- public/templates/slider.php
+++ public/templates/slider.php
@@ -1,12 +1,10 @@
-<div class="slider">
-  <img xmlId="<?php echo $project->images[0]->id ?>" src="<?php echo $project->images[0]->src ?>"/>
+<div class="slider" projId="<?php echo $project->id ?>" >
+  <div class="sliderImage" xmlId="<?php echo $project->images[0]->id ?>" style="background-image:url(<?php echo $project->images[0]->src ?>);" >
+    <div direction="prev" class="darkDiv sliderBtn left"><div>Prev</div></div>
+    <div direction="next" class="darkDiv sliderBtn right"><div>Next</div></div>
+    <div class="clear"></div>
+  </div>
   <br/>
-  <button projId="<?php echo $project->id ?>" direction="prev" class="sliderBtn">
-    Prev
-  </button>
-  <button projId="<?php echo $project->id ?>" direction="next" class="sliderBtn right">
-    Next
-  </button>
   <div class="clear"></div>
   <h4 class="title"><?php echo $project->images[0]->title ?></h4>
   <p class="caption"><?php echo $project->images[0]->caption ?></p>
